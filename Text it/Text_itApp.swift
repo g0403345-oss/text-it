c389f43517bbdf5b29ac12f83d725764ada789b8
Norecoil.de <support@norecoill.de>
2026-05-11 20:45:38 +0200

    Initial Commit

---
+++ Text it/Text_itApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_itApp.swift
+//  Text it
+//
+//  Created by Justin Gül on 11.05.26.
+//
+
+import SwiftUI
+
+@main
+struct Text_itApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
